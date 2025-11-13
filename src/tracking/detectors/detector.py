395071dd153143ed7ee2d806de7c13bcefefddc4
hectorgarciaa <hectorgarciarincon20@gmail.com>
2025-11-13 17:32:44 +0100

    Update detector.py

--- src/tracking/detectors/detector.py
+++ src/tracking/detectors/detector.py
@@ -6,7 +6,4 @@ class Detector:
         self.conf = conf
     
     def detect(self, video, stream=True):
-        return self.model.predict(video, stream=stream, conf=self.conf, verbose=False)
-        
-
-
+        return self.model.predict(video, stream=stream, conf=self.conf)
